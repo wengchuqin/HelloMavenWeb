e551ee2003b137483a19a8d5ae12722f36681cf1
chuqin <chuqinweng@qq.com>
2017-12-20 18:57:11 +0800

    Initial commit

---
+++ src/main/java/Service.java
@@ -0,0 +1,23 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wengchuqin on 2017/12/20.
+ */
+public class Service {
+    public void hello() throws FileNotFoundException {
+        System.out.println("hello");
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        FileInputStream stream = new FileInputStream("222");
+        list.add(1);
+        list.add(2);
+    }
+
+    public void hello2(){
+
+    }
+}
